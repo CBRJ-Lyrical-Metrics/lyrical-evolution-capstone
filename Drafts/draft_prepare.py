a8f5018ef39aa3cf417b04e937621a88b777e655
Chris Teceno <chris.teceno@gmail.com>
2022-06-02 11:58:10 -0500

    update prepare.py

--- Drafts/draft_prepare.py
+++ Drafts/draft_prepare.py
@@ -134,6 +134,7 @@ def lemmatize(string):
 
 ################### CLEAN DATAFRAME ###################
 
+
 def clean_df(df, extra_words=[], exclude_words=[]):
     """
     remove nulls,
@@ -148,6 +149,8 @@ def clean_df(df, extra_words=[], exclude_words=[]):
     remove stopwords,
     lemma
     """
+    # make column for raw unprocessed lyrics
+    df["raw_lyrics"] = df["lyrics"]
     # drop nulls
     df.dropna(inplace=True)
     # convert date to datetime
@@ -169,63 +172,185 @@ def clean_df(df, extra_words=[], exclude_words=[]):
     # remove stopwords
     df["lyrics"] = df.lyrics.apply(remove_stopwords)
     # lemmatize
+    print("Lemmatizing...", end="\r")
     df["lyrics"] = df.lyrics.apply(lemmatize)
+    # if df has column 'Unnamed: 0', remove it
+    # this allows for the csv to be pulled in different ways
+    if "Unnamed: 0" in df.columns:
+        # # drop unnamed column
+        df = df.drop(columns=["Unnamed: 0"])
+
+    # below is added to 'add_features()'
     # create columns with character and word counts
+
+    # moved to add_features()
+    # df = df.assign(
+    #     character_count=df.lyrics.str.len(),
+    #     word_count=df.lyrics.str.split().apply(len),
+    # )
+
+    # # sentiment measurement
+    # df["sentiment"] = df.lyrics.apply(
+    #     lambda msg: sia.polarity_scores(msg)["compound"]
+    # )
+    return df
+
+
+def add_features(df):
+    """add features for EDA"""
+    # create columns with character and word counts
+    print("add word and char counts...", end="\r")
+
     df = df.assign(
         character_count=df.lyrics.str.len(),
         word_count=df.lyrics.str.split().apply(len),
     )
-    # drop unnamed column
-    df = df.drop(columns = ['Unnamed: 0'])
-    
-    # sentiment measurement 
-    df["sentiment"] = df.lyrics.apply(
-        lambda msg: sia.polarity_scores(msg)["compound"]
-    )
+    # sentiment measurement
+    print("adding sentiment **********", end="\r")
+    df["sentiment"] = df.lyrics.apply(lambda msg: sia.polarity_scores(msg)["compound"])
+    # get place words (chorus, lyrics, etc)
+    print("adding place words ********", end="\r")
+    df = get_place_words(df)
+    # add column for chorus count
+    print("adding chorus count ********", end="\r")
+    df["chorus_count"] = df.place_words.apply(lambda x: x.count("[chorus"))
+    # add column for verse count
+    print("adding verse count *********", end="\r")
+    df["verse_count"] = df.place_words.apply(lambda x: x.count("verse"))
+    # add column for ratio of verses to chorus
+    print("adding verse to chorus ratio", end="\r")
+    df["verse_chorus_ratio"] = df.verse_count / df.chorus_count
+    # add column for pre-chorus count
+    print("adding pre-chorus count ****", end="\r")
+    df["pre_chorus_count"] = df.place_words.apply(lambda x: x.count("[pre-chorus"))
+    # add column for outro count
+    print("adding outro count *********", end="\r")
+    df["outro_count"] = df.place_words.apply(lambda x: x.count("outro"))
+    # add column for bridge count
+    print("adding bridge count ********", end="\r")
+    df["bridge_count"] = df.place_words.apply(lambda x: x.count("bridge"))
+    # add column for hook count
+    print("adding hook count **********", end="\r")
+    df["hook_count"] = df.place_words.apply(lambda x: x.count("hook"))
+    # fill NaN in with 0
+    print("filling NaN ratio with 0 ***", end="\r")
+    df.verse_chorus_ratio.fillna(0, inplace=True)
+    # add n_grams
+    print("adding n_grams *************", end="\r")
+    df = get_n_grams(df)
+    # add column for n_grams count
+    print("adding n_grams count ********", end="\r")
+    df["n_grams_count"] = df.n_grams.apply(lambda x: len(x))
+    # add column for unique words
+    print("adding unique words *********", end="\r")
+    df["unique_words"] = df.lyrics.apply(get_unique_words)
+    # add column for unique words count
+    print("adding unique words count ****", end="\r")
+    df["unique_words_count"] = df.unique_words.apply(lambda x: len(x.split()))
+
     return df
+
+
+def get_n_grams(df):
+    """
+    get bigrams/trigrams for each song
+    """
+    # create bigram column
+    print("creating bigram column******", end="\r")
+    df["bigrams"] = df.lyrics.apply(lambda x: list(nltk.ngrams(x.split(), 2)))
+    # create bigram column
+    print("creating trigram column*****", end="\r")
+    df["trigrams"] = df.lyrics.apply(lambda x: list(nltk.ngrams(x.split(), 3)))
+    return df
+
+
+def get_place_words(df):
+    """ get the tags such as 'chorus' 'verse' and 'hook' """
+    # get all words in [] from raw_lyrics
+    df["place_words"] = df.raw_lyrics.apply(lambda x: re.findall(r"\[.*?\]", x))
+    # make place_words all lowercase
+    df["place_words"] = df.place_words.apply(lambda x: [word.lower() for word in x])
+    # make place_words a string
+    df["place_words"] = df.place_words.apply(lambda x: " ".join(x))
+    return df
+
+
+def get_unique_words(text):
+    """
+    Get unique words in text
+    """
+
+    # Create a list of words separated by a space
+    words = text.split()
+    # Create a variable to hold a list of unique words
+    unique_words = set(words)
+    # Join unique words list with a new string
+    new_string = " ".join(unique_words)
+    return new_string
 
 
 ############################### Adding Topics ###############################
 def get_topics(df):
-    np.random.seed(42)  
+    np.random.seed(42)
     # Create an instance
-    cv = CountVectorizer(max_df = .95, min_df = 2, stop_words = 'english')
-    
+    cv = CountVectorizer(max_df=0.95, min_df=2, stop_words="english")
+
     # Fit and transform the lemmatized lyrics data
     cv_fit = cv.fit_transform(df.lyrics)
 
     # Create the instance for LDA
-    lda = LatentDirichletAllocation(n_components = 20, random_state = 42)
-    
+    lda = LatentDirichletAllocation(n_components=20, random_state=42)
+
     # Fit the vectorizer with the LDA
     lda.fit(cv_fit)
-    
+
     # Pull feature names out and define as feature
     feature = cv.get_feature_names()
-    
+
     # Final df transforming cv_fit
     df_final = lda.transform(cv_fit)
-    
-#     # Make copy to save original df 
-#     df_new = copy.deepcopy(df)
-    
+
+    #     # Make copy to save original df
+    #     df_new = copy.deepcopy(df)
+
     prob = df_final[0][df_final[0].argmax()].round(2)
-    
+
     # Assign the opics tp the dataframe
-    df['topic'] = df_final.argmax(axis = 1)
-    
+    df["topic"] = df_final.argmax(axis=1)
+
     # Creating a dictionary with key as topic numbers and value as topic names
-    topic_label = {0:'jealousy', 1:'affection', 2:'breakups', 3:'dance', 4:'holiday', 5:'nature', 
-                   6:'spanish', 7:'transcendental', 8:'lost', 9:'violence', 10:'youth', 11:'love', 12:'heartache', 
-                   13:'money', 14:'affection', 15:'sex', 16:'dance', 17:'good vibes', 18:'americana', 19:'breakup'}
-    
+    topic_label = {
+        0: "jealousy",
+        1: "affection",
+        2: "breakups",
+        3: "dance",
+        4: "holiday",
+        5: "nature",
+        6: "spanish",
+        7: "transcendental",
+        8: "lost",
+        9: "violence",
+        10: "youth",
+        11: "love",
+        12: "heartache",
+        13: "money",
+        14: "affection",
+        15: "sex",
+        16: "dance",
+        17: "good vibes",
+        18: "americana",
+        19: "breakups",
+    }
+
     # Mapping the dictionary with the dataframe to get the labels column.
-    df['topic_name'] = df['topic'].map(topic_label)
+    df["topic_name"] = df["topic"].map(topic_label)
     # Drop unnecessary column 'topic'
-    df = df.drop(columns = ['topic']) 
+    df = df.drop(columns=["topic"])
     return df
 
+
 ###############################  ###############################
+
 
 def model_clean(df):
     """
@@ -265,7 +390,7 @@ def model_clean(df):
     df["lyrics"] = df.lyrics.apply(remove_stopwords)
     # lemmatize
     df["lyrics"] = df.lyrics.apply(lemmatize)
-    
+
     return df
 
 
